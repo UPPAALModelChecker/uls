1c2ed33290d4583f1e67b5fc36090ceca122b919
Thorulf Neustrup <thorulf@live.dk>
2023-11-07 14:01:06 +0100

    Fixed bad WError issues in libraries

--- cmake/nlohmann.cmake
+++ cmake/nlohmann.cmake
@@ -4,4 +4,10 @@ FetchContent_Declare(json
   URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
   DOWNLOAD_EXTRACT_TIMESTAMP ON
 )
-FetchContent_MakeAvailable(json)
+
+# Manually make available so we can mark library as SYSTEM headers thus disabling warnings
+FetchContent_GetProperties(json)
+if(NOT json_POPULATED)
+  FetchContent_Populate(json)
+  add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} SYSTEM)
+endif()
