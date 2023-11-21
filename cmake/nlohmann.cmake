25b16caa9c9b372ec72200fdd2c2ce722b11080e
Thorulf Neustrup <thorulf@live.dk>
2023-11-21 11:28:42 +0100

    Disabled Werror and remove System tag for json lib

--- cmake/nlohmann.cmake
+++ cmake/nlohmann.cmake
@@ -1,13 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(json
-  URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
   DOWNLOAD_EXTRACT_TIMESTAMP ON
+  URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
 )
-
-# Manually make available so we can mark library as SYSTEM headers thus disabling warnings
-FetchContent_GetProperties(json)
-if(NOT json_POPULATED)
-  FetchContent_Populate(json)
-  add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} SYSTEM)
-endif()
+FetchContent_MakeAvailable(json)
